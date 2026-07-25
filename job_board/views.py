95e1a04a582bfe56d2eda18907a05bb7aef55550
alireza <alirezaariye@gmail.com>
2026-07-25 22:06:11 +0330

    feat: add is_active field to JobPosting model and update index view to filter active jobs

--- job_board/views.py
+++ job_board/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
+from .models import JobPosting
+
 
 # Create your views here.
 def index(request):
+    # jobs = JobPosting.objects.all()
+    jobs = JobPosting.objects.filter(is_active=True)
+    print(jobs)
     return HttpResponse("<h1>hi</h1>")
