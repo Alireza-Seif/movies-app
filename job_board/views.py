7aa48fd9944321dc2aab95bdf566e6a17d956fb5
alireza <alirezaariye@gmail.com>
2026-07-25 22:18:22 +0330

    feat: implement index view to display active job postings in HTML template

--- job_board/views.py
+++ job_board/views.py
@@ -1,11 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import JobPosting
 
 
 # Create your views here.
 def index(request):
-    # jobs = JobPosting.objects.all()
-    jobs = JobPosting.objects.filter(is_active=True)
-    print(jobs)
-    return HttpResponse("<h1>hi</h1>")
+    active_postings = JobPosting.objects.filter(is_active=True)
+    context = {"job_postings": active_postings}
+    return render(request, "job_board/index.html", context)
