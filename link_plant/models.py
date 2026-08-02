0863114efa71136345378a398af42e5e2a69a2f2
alireza <alirezaariye@gmail.com>
2026-08-02 18:25:31 +0330

    feat: update admin registration and enhance Link model string representation

--- link_plant/models.py
+++ link_plant/models.py
@@ -13,8 +13,6 @@ class Profile(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     bg_color = models.CharField(max_length=50, choices=BG_CHOICES)
-    
-    
 
     def __str__(self):
         return f"{self.name}"
@@ -24,3 +22,6 @@ class Link(models.Model):
     text = models.CharField(max_length=100)
     url = models.URLField()
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="links")
+
+    def __str__(self):
+        return f"{self.profile} | {self.url}"
