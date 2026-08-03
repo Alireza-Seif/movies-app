5068a14aefc3639772b554f147edda1e73a1861d
alireza <alirezaariye@gmail.com>
2026-08-03 19:48:02 +0330

    feat: add LinkUpdateView for updating links and update URL routing

--- link_plant/views.py
+++ link_plant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Profile, Link
@@ -14,4 +14,9 @@ class LinkCreateView(CreateView):
     model = Link
     fields = "__all__"
     success_url = reverse_lazy("link-list")
-    
+
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    fields = ["text", "url"]
+    success_url = reverse_lazy("link-list") 
